9e231eed757350cb4598f215f730d4d3b3926cb9
Rishabh Kamboj <heyrishabh13@gmail.com>
2025-09-29 17:21:59 +0530

    Get request with html folder added

---
+++ Get-request-with-html/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const productRouter = require("./routes/product");
+
+const app = express();
+
+app.use("/api/product", productRouter);
+
+app.listen(3000, () => {
+  console.log(`Server is listening on http://localhost:3000`);
+});
